844020bf5127c3cd75b395caf2ef790f3048c40b
Allen Li <darkfeline@felesatra.moe>
2024-10-08 15:58:36 -0700

    Add ddskk 20230701.2340

---
+++ ddskk-20230701.2340/ddskk-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "ddskk" "20230701.2340"
+  "Simple Kana to Kanji conversion program."
+  '((ccc "1.43")
+    (cdb "20141201.754"))
+  :url "https://github.com/skk-dev/ddskk"
+  :commit "8c47f46e38a29a0f3eabcd524268d20573102467"
+  :revdesc "8c47f46e38a2"
+  :keywords '("japanese" "mule" "input method")
+  :authors '(("Masahiko Sato" . "masahiko@kuis.kyoto-u.ac.jp")))
