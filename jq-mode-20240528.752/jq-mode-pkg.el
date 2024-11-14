be12e6cfe81c3c03c48b61514f97d33aed33e612
Allen Li <darkfeline@felesatra.moe>
2024-11-13 18:26:15 -0800

    Add jq-mode 20240528.752

---
+++ jq-mode-20240528.752/jq-mode-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "jq-mode" "20240528.752"
+  "Edit jq scripts."
+  '((emacs "25.1"))
+  :url "https://github.com/ljos/jq-mode"
+  :commit "a0f79eba786d46f01aeabb5913aadc337e985d6c"
+  :revdesc "a0f79eba786d"
+  :authors '(("Bjarte Johansen" . "BjartedotJohansenatgmaildotcom"))
+  :maintainers '(("Bjarte Johansen" . "BjartedotJohansenatgmaildotcom")))
