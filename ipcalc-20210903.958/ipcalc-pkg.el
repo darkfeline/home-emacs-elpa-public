606ad71d54c570f89c86b6b4c9460206ebfe79aa
Allen Li <darkfeline@felesatra.moe>
2024-11-14 14:06:08 -0800

    Add ipcalc 20210903.958

---
+++ ipcalc-20210903.958/ipcalc-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "ipcalc" "20210903.958"
+  "IP subnet calculator."
+  '((cl-lib "0.5"))
+  :url "https://github.com/dotemacs/ipcalc.el"
+  :commit "05fcb5bb8db3ba0c1f9e5f1bfcf0c183828a2426"
+  :revdesc "05fcb5bb8db3"
+  :keywords '("networking" "tools")
+  :authors '(("Aleksandar Simic" . "asimic@gmail.com"))
+  :maintainers '(("Aleksandar Simic" . "asimic@gmail.com")))
