baa2b3ff38b069a74c84225a8969a53411292d14
Allen Li <darkfeline@felesatra.moe>
2024-11-14 23:39:31 -0800

    Add tramp 2.7.1.4

---
+++ tramp-2.7.1.4/tramp-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from tramp.el  -*- no-byte-compile: t -*-
+(define-package "tramp" "2.7.1.4" "Transparent Remote Access, Multiple Protocol" '((emacs "27.1")) :commit "db8ba2f51989a33a5150fe89766eb9d376b0f843" :authors '(("Kai Großjohann" . "kai.grossjohann@gmx.net")) :maintainer '("Michael Albinus" . "michael.albinus@gmx.de") :keywords '("comm" "processes") :url "https://www.gnu.org/software/tramp/")
