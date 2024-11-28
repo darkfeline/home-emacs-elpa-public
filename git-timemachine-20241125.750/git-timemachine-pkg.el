dea5c7a4b02df6ef47c52905b7f88b3486b58cf7
Allen Li <darkfeline@felesatra.moe>
2024-11-27 22:45:51 -0800

    Update git-timemachine 20240427.924 -> 20241125.750

--- git-timemachine-20241125.750/git-timemachine-pkg.el
+++ git-timemachine-20241125.750/git-timemachine-pkg.el
@@ -1,11 +1,11 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "git-timemachine" "20240427.924"
+(define-package "git-timemachine" "20241125.750"
   "Walk through git revisions of a file."
   '((emacs     "24.3")
     (transient "0.1.0"))
   :url "https://codeberg.org/pidu/git-timemachine"
-  :commit "3780835fcd67c3703ffa768206121851e6895ece"
-  :revdesc "3780835fcd67"
+  :commit "62ebb0c6588d848b1b7b90bf1a3aa4c04e873822"
+  :revdesc "62ebb0c6588d"
   :keywords '("vc")
   :authors '(("Peter Stiernström" . "peter@stiernstrom.se"))
   :maintainers '(("Peter Stiernström" . "peter@stiernstrom.se")))
