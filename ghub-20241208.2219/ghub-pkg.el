f4773acd1de16bc7f45d8a9689ca19b9507ac36e
Allen Li <darkfeline@felesatra.moe>
2024-12-27 13:20:00 -0800

    Update ghub 20241123.2135 -> 20241208.2219

--- ghub-20241208.2219/ghub-pkg.el
+++ ghub-20241208.2219/ghub-pkg.el
@@ -1,13 +1,13 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "ghub" "20241123.2135"
+(define-package "ghub" "20241208.2219"
   "Client libraries for Git forge APIs."
-  '((emacs     "26.3")
+  '((emacs     "29.1")
     (compat    "30.0.0.0")
     (let-alist "1.0.6")
     (treepy    "0.1.2"))
   :url "https://github.com/magit/ghub"
-  :commit "4f39b86544ab064204efd0dda381af90fae70f43"
-  :revdesc "4f39b86544ab"
+  :commit "97edaf450ef74f40e6c0bd6a35ebd3fcb710ca4d"
+  :revdesc "97edaf450ef7"
   :keywords '("tools")
   :authors '(("Jonas Bernoulli" . "emacs.ghub@jonas.bernoulli.dev"))
   :maintainers '(("Jonas Bernoulli" . "emacs.ghub@jonas.bernoulli.dev")))
