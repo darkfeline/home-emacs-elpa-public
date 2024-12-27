d32e9561c13aec4d166432fdce681a6405083d51
Allen Li <darkfeline@felesatra.moe>
2024-12-27 13:18:32 -0800

    Update transient 0.7.9 -> 0.8.1

---
+++ transient-0.8.1/transient-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from transient.el  -*- no-byte-compile: t -*-
+(define-package "transient" "0.8.1" "Transient commands" '((emacs "26.1") (compat "30.0.0.0") (seq "2.24")) :commit "dbe18e3f5e0812b8d086f92ca776935e41fafc75" :authors '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev")) :maintainer '("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev") :keywords '("extensions") :url "https://github.com/magit/transient")
