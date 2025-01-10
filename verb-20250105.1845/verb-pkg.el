d57d2e2cc4fad7b1754ae1505464e8929d730dc9
Allen Li <darkfeline@felesatra.moe>
2025-01-09 16:23:20 -0800

    Update verb 20241223.1316 -> 20250105.1845

--- verb-20250105.1845/verb-pkg.el
+++ verb-20250105.1845/verb-pkg.el
@@ -1,10 +1,10 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "verb" "20241223.1316"
+(define-package "verb" "20250105.1845"
   "Organize and send HTTP requests."
   '((emacs "26.3"))
   :url "https://github.com/federicotdn/verb"
-  :commit "2c46542a64e79919496f5a8255b7321f6ba00fd1"
-  :revdesc "2c46542a64e7"
+  :commit "13ddd3d5339ba50f25662bed147ff02614e0c306"
+  :revdesc "13ddd3d5339b"
   :keywords '("tools")
   :authors '(("Federico Tedin" . "federicotedin@gmail.com"))
   :maintainers '(("Federico Tedin" . "federicotedin@gmail.com")))
