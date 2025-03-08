9fa096e27f82fe2fa15abfb1fb9f8696b794cda9
Allen Li <darkfeline@felesatra.moe>
2025-03-07 16:22:31 -0800

    Update yaml 20241129.2114 -> 20250208.1534

--- yaml-20250208.1534/yaml-pkg.el
+++ yaml-20250208.1534/yaml-pkg.el
@@ -1,10 +1,10 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "yaml" "20241129.2114"
+(define-package "yaml" "20250208.1534"
   "YAML parser for Elisp."
   '((emacs "25.1"))
   :url "https://github.com/zkry/yaml.el"
-  :commit "cd3edfc02cb12514426c00e07160b87bd8340f4a"
-  :revdesc "cd3edfc02cb1"
+  :commit "09e46d563f1f3ff948852e08360c7d3c76e2acba"
+  :revdesc "09e46d563f1f"
   :keywords '("tools")
   :authors '(("Zachary Romero" . "zkry@posteo.org"))
   :maintainers '(("Zachary Romero" . "zkry@posteo.org")))
