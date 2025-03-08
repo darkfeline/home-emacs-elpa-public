80f9a701753e257571fe28500730f975ca82c9f5
Allen Li <darkfeline@felesatra.moe>
2025-03-07 16:23:35 -0800

    Add llama 20250307.1729

---
+++ llama-20250307.1729/llama-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "llama" "20250307.1729"
+  "Compact syntax for short lambda."
+  '((emacs  "26.1")
+    (compat "30.0.2.0"))
+  :url "https://github.com/tarsius/llama"
+  :commit "580ad210e01cb54511aa399614e26feceeb17aaa"
+  :revdesc "580ad210e01c"
+  :keywords '("extensions"))
