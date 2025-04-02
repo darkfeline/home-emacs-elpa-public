5390a56adbda3a7475e148a74b14b32015acccff
Allen Li <darkfeline@felesatra.moe>
2025-04-01 22:33:02 -0700

    Update ghub 20250301.1641 -> 20250401.1509

--- ghub-20250401.1509/ghub-pkg.el
+++ ghub-20250401.1509/ghub-pkg.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "ghub" "20250301.1641"
+(define-package "ghub" "20250401.1509"
   "Client libraries for Git forge APIs."
   '((emacs     "29.1")
     (compat    "30.0.2.0")
@@ -7,8 +7,8 @@
     (llama     "0.6.1")
     (treepy    "0.1.2"))
   :url "https://github.com/magit/ghub"
-  :commit "af663777c47a3dce64b2144b4409587b35521e47"
-  :revdesc "af663777c47a"
+  :commit "1fbce5379e21565f497c0f59bbe5349773c4be62"
+  :revdesc "1fbce5379e21"
   :keywords '("tools")
   :authors '(("Jonas Bernoulli" . "emacs.ghub@jonas.bernoulli.dev"))
   :maintainers '(("Jonas Bernoulli" . "emacs.ghub@jonas.bernoulli.dev")))
