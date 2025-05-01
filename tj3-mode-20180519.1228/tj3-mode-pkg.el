928868046b57f88dfad36751caa1de3bdf5eb314
Allen Li <darkfeline@felesatra.moe>
2025-05-01 15:49:57 -0700

    Add tj3-mode 20180519.1228

---
+++ tj3-mode-20180519.1228/tj3-mode-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "tj3-mode" "20180519.1228"
+  "Major mode for editing TaskJuggler 3 files."
+  ()
+  :url "https://github.com/csrhodes/tj3-mode"
+  :commit "1d98eb23f1606392f34ef1b80517cfc940fb9950"
+  :revdesc "1d98eb23f160"
+  :authors '(("Christophe Rhodes" . "christophe@rhodes.io"))
+  :maintainers '(("Christophe Rhodes" . "christophe@rhodes.io")))
