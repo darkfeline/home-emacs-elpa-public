c5e30715053c59c1e00dcaed4f3d7a740df3e4ad
Allen Li <darkfeline@felesatra.moe>
2025-05-28 20:29:46 +0000

    Update org 9.7.27 -> 9.7.30

--- org-9.7.30/org-version.el
+++ org-9.7.30/org-version.el
@@ -5,13 +5,13 @@
 (defun org-release ()
   "The release version of Org.
 Inserted by installing Org mode or when a release is made."
-   (let ((org-release "9.7.27"))
+   (let ((org-release "9.7.30"))
      org-release))
 ;;;###autoload
 (defun org-git-version ()
   "The Git version of Org mode.
 Inserted by installing Org or when a release is made."
-   (let ((org-git-version "9.7.27-74dde3"))
+   (let ((org-git-version "9.7.30-84f18c"))
      org-git-version))
 
 (provide 'org-version)
