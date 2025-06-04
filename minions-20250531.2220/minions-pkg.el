a30fbed583b719402aa40d9811bc03bc1940d072
Allen Li <darkfeline@felesatra.moe>
2025-06-04 21:47:56 +0000

    Update minions 20250101.1417 -> 20250531.2220

--- minions-20250531.2220/minions-pkg.el
+++ minions-20250531.2220/minions-pkg.el
@@ -1,11 +1,11 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "minions" "20250101.1417"
+(define-package "minions" "20250531.2220"
   "A minor-mode menu for the mode line."
   '((emacs  "26.1")
-    (compat "30.0.1.0"))
+    (compat "30.1"))
   :url "https://github.com/tarsius/minions"
-  :commit "7ccb5e23a54c10f64880d1f55676f86681ff2f07"
-  :revdesc "7ccb5e23a54c"
+  :commit "7aeabcab964fefacb15a72d983953c53e238d344"
+  :revdesc "7aeabcab964f"
   :keywords '("convenience")
   :authors '(("Jonas Bernoulli" . "emacs.minions@jonas.bernoulli.dev"))
   :maintainers '(("Jonas Bernoulli" . "emacs.minions@jonas.bernoulli.dev")))
