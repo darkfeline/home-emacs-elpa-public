8358b8ffead394aefac2f4d3725707f273fb0f60
Allen Li <darkfeline@felesatra.moe>
2025-07-16 00:51:31 +0000

    Add gptel-commit 20250630.1324

---
+++ gptel-commit-20250630.1324/gptel-commit-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "gptel-commit" "20250630.1324"
+  "Generate commit message with gptel."
+  '((emacs "27.1")
+    (gptel "0.9.8"))
+  :url "https://github.com/lakkiy/gptel-commit"
+  :commit "9caf217c9864c3f4cf819c2c48d35628ac729427"
+  :revdesc "9caf217c9864"
+  :keywords '("vc" "convenience")
+  :authors '(("Liu Bo" . "liubolovelife@gmail.com"))
+  :maintainers '(("Liu Bo" . "liubolovelife@gmail.com")))
