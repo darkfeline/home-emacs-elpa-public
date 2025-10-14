b88ce5bb3836629799a36ad99e1378207f03746a
Allen Li <darkfeline@felesatra.moe>
2025-10-14 00:59:56 -0700

    Add mcp 20251010.548

---
+++ mcp-20251010.548/mcp-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "mcp" "20251010.548"
+  "Model Context Protocol."
+  '((emacs   "30.1")
+    (jsonrpc "1.0.25"))
+  :url "https://github.com/lizqwerscott/mcp.el"
+  :commit "6132ffc05e8ba5ac188b7f6b54a0fd3192f110d1"
+  :revdesc "6132ffc05e8b"
+  :keywords '("tools")
+  :authors '(("lizqwer scott" . "lizqwerscott@gmail.com"))
+  :maintainers '(("lizqwer scott" . "lizqwerscott@gmail.com")))
