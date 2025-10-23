fce45102be612ba190a53e47572f91c13ea2e2aa
Allen Li <darkfeline@felesatra.moe>
2025-10-22 17:29:53 -0700

    Add gptel-magit 20250520.833

---
+++ gptel-magit-20250520.833/gptel-magit-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "gptel-magit" "20250520.833"
+  "Generate commit messages for magit using gptel."
+  '((emacs "28.1")
+    (magit "4.0")
+    (gptel "0.9.8"))
+  :url "https://github.com/ragnard/gptel-magit"
+  :commit "f27c01821b67ed99ddf705c2b995f78b71394d8b"
+  :revdesc "f27c01821b67"
+  :keywords '("vc" "convenience")
+  :authors '(("Ragnar Dahlén" . "r.dahlen@gmail.com"))
+  :maintainers '(("Ragnar Dahlén" . "r.dahlen@gmail.com")))
