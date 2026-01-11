4642b6db6414450070e2cd13b8cc85763c68283f
Allen Li <darkfeline@felesatra.moe>
2026-01-10 23:17:07 -0800

    Update editorconfig 20250905.724 -> 20251221.650

--- editorconfig-20251221.650/editorconfig-pkg.el
+++ editorconfig-20251221.650/editorconfig-pkg.el
@@ -1,10 +1,10 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "editorconfig" "20250905.724"
+(define-package "editorconfig" "20251221.650"
   "EditorConfig Emacs Plugin."
   '((emacs "27.2"))
   :url "https://github.com/editorconfig/editorconfig-emacs#readme"
-  :commit "4af10445fcdf1c9dfa7af4f9e5bec59e8a759d6f"
-  :revdesc "4af10445fcdf"
+  :commit "6783cb2a9fb82dc152e633e2e40162c150a4e818"
+  :revdesc "6783cb2a9fb8"
   :keywords '("convenience" "editorconfig")
   :authors '(("EditorConfig Team" . "editorconfig@googlegroups.com"))
   :maintainers '(("EditorConfig Team" . "editorconfig@googlegroups.com")))
