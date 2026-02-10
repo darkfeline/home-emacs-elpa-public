b67209d6f9bf641fbf1413c75fdf8f46c3af27b4
Allen Li <darkfeline@felesatra.moe>
2026-02-10 14:47:12 -0800

    Update tramp 2.8.1 -> 2.8.1.1

---
+++ tramp-2.8.1.1/tramp-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from tramp.el  -*- mode: lisp-data; no-byte-compile: t -*-
+(define-package "tramp" "2.8.1.1" "Transparent Remote Access, Multiple Protocol" '((emacs "28.1")) :commit "c04cee076dc5ed04abef538cde268d388f9571df" :authors '(("Kai Großjohann" . "kai.grossjohann@gmx.net") ("Michael Albinus" . "michael.albinus@gmx.de")) :maintainer '("Michael Albinus" . "michael.albinus@gmx.de") :keywords '("comm" "processes") :url "https://www.gnu.org/software/tramp/")
