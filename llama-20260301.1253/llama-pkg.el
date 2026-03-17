09121c518d503c6bea260fcb723a2342d7633839
Allen Li <darkfeline@felesatra.moe>
2026-03-17 14:24:53 -0700

    Update llama 20260101.1830 -> 20260301.1253

--- llama-20260301.1253/llama-pkg.el
+++ llama-20260301.1253/llama-pkg.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "llama" "20260101.1830"
+(define-package "llama" "20260301.1253"
   "Compact syntax for short lambda."
   '((emacs  "26.1")
     (compat "30.1"))
   :url "https://github.com/tarsius/llama"
-  :commit "2a89ba755b0459914a44b1ffa793e57f759a5b85"
-  :revdesc "2a89ba755b04"
+  :commit "d430d48e0b5afd2a34b5531f103dcb110c3539c4"
+  :revdesc "d430d48e0b5a"
   :keywords '("extensions"))
