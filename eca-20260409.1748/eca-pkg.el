e9bbd3ba620c16ea6c87e7da991d7d5194b1a3b6
Allen Li <darkfeline@felesatra.moe>
2026-04-11 00:00:23 +0000

    Add eca 20260409.1748

---
+++ eca-20260409.1748/eca-pkg.el
@@ -0,0 +1,15 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "eca" "20260409.1748"
+  "AI pair programming via ECA (Editor Code Assistant)."
+  '((emacs         "28.1")
+    (dash          "2.18.0")
+    (s             "1.12.0")
+    (f             "0.20.0")
+    (markdown-mode "2.3")
+    (compat        "30.1"))
+  :url "https://github.com/editor-code-assistant/eca-emacs"
+  :commit "73d77d635e0cc6daecef879b32057a6008a724ee"
+  :revdesc "73d77d635e0c"
+  :keywords '("tools")
+  :authors '(("Eric Dallo" . "ercdll1337@gmail.com"))
+  :maintainers '(("Eric Dallo" . "ercdll1337@gmail.com")))
