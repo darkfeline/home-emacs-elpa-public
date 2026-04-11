39561e03ef7860505ac1ee1fead1a9ada2f52ac4
Allen Li <darkfeline@felesatra.moe>
2026-04-10 23:02:32 -0700

    Add gptel-agent 20260411.214

---
+++ gptel-agent-20260411.214/gptel-agent-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "gptel-agent" "20260411.214"
+  "Agentic LLM use for gptel."
+  '((emacs     "29.1")
+    (compat    "30.1.0.0")
+    (gptel     "0.9.9")
+    (yaml      "1.2.0")
+    (orderless "1.1"))
+  :url "https://github.com/karthink/gptel-agent"
+  :commit "1811b991552e6b24f67c5af369ebb361995518b3"
+  :revdesc "1811b991552e"
+  :keywords '("comm"))
