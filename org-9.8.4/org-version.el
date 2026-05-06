f9b89b52b62a3b66273638711014f37279750e1f
Allen Li <darkfeline@felesatra.moe>
2026-05-06 23:20:56 +0000

    Update org 9.8.3 -> 9.8.4

--- org-9.8.4/org-version.el
+++ org-9.8.4/org-version.el
@@ -5,13 +5,13 @@
 (defun org-release ()
   "The release version of Org.
 Inserted by installing Org mode or when a release is made."
-   (let ((org-release "9.8.3"))
+   (let ((org-release "9.8.4"))
      org-release))
 ;;;###autoload
 (defun org-git-version ()
   "The Git version of Org mode.
 Inserted by installing Org or when a release is made."
-   (let ((org-git-version "9.8.3-5d7116"))
+   (let ((org-git-version "9.8.4-2f6959"))
      org-git-version))
 
 (provide 'org-version)
