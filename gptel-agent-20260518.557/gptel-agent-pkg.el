4fcdbf327c69d45087124d73ac7a85dae7c7ba10
Allen Li <darkfeline@felesatra.moe>
2026-05-22 01:28:24 -0700

    Update gptel-agent 20260502.1809 -> 20260518.557

--- gptel-agent-20260518.557/gptel-agent-pkg.el
+++ gptel-agent-20260518.557/gptel-agent-pkg.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "gptel-agent" "20260502.1809"
+(define-package "gptel-agent" "20260518.557"
   "Agentic LLM use for gptel."
   '((emacs     "29.1")
     (compat    "30.1.0.0")
@@ -7,6 +7,6 @@
     (yaml      "1.2.0")
     (orderless "1.1"))
   :url "https://github.com/karthink/gptel-agent"
-  :commit "aecaa940faf29107b4bc0a2bd621419c2d04b8d0"
-  :revdesc "aecaa940faf2"
+  :commit "753e722778fcdefc165f049d27cbfea4fb909236"
+  :revdesc "753e722778fc"
   :keywords '("comm"))
