a788c74c0e032efd2c6ee8a4284177fbf28070e4
Allen Li <darkfeline@felesatra.moe>
2026-06-21 02:32:34 -0700

    Update dumb-jump 20260406.1824 -> 20260603.1700

--- dumb-jump-20260603.1700/dumb-jump-pkg.el
+++ dumb-jump-20260603.1700/dumb-jump-pkg.el
@@ -1,8 +1,8 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "dumb-jump" "20260406.1824"
+(define-package "dumb-jump" "20260603.1700"
   "Jump to definition for 60+ languages without configuration."
   '((emacs "26.1"))
   :url "https://github.com/jacktasia/dumb-jump"
-  :commit "9ce4598e9c485821a6e639fa48854d8e05acd970"
-  :revdesc "9ce4598e9c48"
+  :commit "cf06b4ccdce6a39346c32f05139f9ee8b77ee229"
+  :revdesc "cf06b4ccdce6"
   :keywords '("programming"))
