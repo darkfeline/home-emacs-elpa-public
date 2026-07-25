8b6b647918c6a8ac500a2d8257a090e7fb363a27
Allen Li <darkfeline@felesatra.moe>
2026-07-24 22:25:53 -0700

    Update prescient 20250816.19 -> 20260628.2243

--- prescient-20260628.2243/prescient-pkg.el
+++ prescient-20260628.2243/prescient-pkg.el
@@ -1,10 +1,10 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "prescient" "20250816.19"
+(define-package "prescient" "20260628.2243"
   "Better sorting and filtering."
   '((emacs "25.1"))
   :url "https://github.com/raxod502/prescient.el"
-  :commit "87e2d2f2ddf24f591a5f70cc90d2afb4537caa18"
-  :revdesc "87e2d2f2ddf2"
+  :commit "5649977fa7789e4615efeca09397ed7eccd06dfc"
+  :revdesc "5649977fa778"
   :keywords '("extensions")
   :authors '(("Radian LLC" . "contact+prescient@radian.codes"))
   :maintainers '(("Radian LLC" . "contact+prescient@radian.codes")))
