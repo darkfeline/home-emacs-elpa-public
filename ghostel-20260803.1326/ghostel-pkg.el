662d00c6d2c40b620b81cb0beba75045fb2e09f5
Allen Li <darkfeline@felesatra.moe>
2026-08-04 21:57:44 +0000

    Add ghostel 20260803.1326

---
+++ ghostel-20260803.1326/ghostel-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "ghostel" "20260803.1326"
+  "Terminal emulator powered by libghostty."
+  '((emacs  "28.1")
+    (compat "30.1.0.1"))
+  :url "https://github.com/dakra/ghostel"
+  :commit "37a82caa25c41fa4fda768ddb727b1cbe117ceba"
+  :revdesc "37a82caa25c4"
+  :keywords '("terminals")
+  :authors '(("Daniel Kraus" . "daniel@kraus.my"))
+  :maintainers '(("Daniel Kraus" . "daniel@kraus.my")))
